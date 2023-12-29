fd92110dbec552c0157dc7e72c73a1a92adc37d0
Meephly <v.sherstyanykh@gmail.com>
2023-12-29 17:36:16 +0300

    fix grave esc

--- keyboards/planck/rev7/keymaps/meephly/keymap.c
+++ keyboards/planck/rev7/keymaps/meephly/keymap.c
@@ -43,12 +43,12 @@ void matrix_scan_user(void) { // The very important timer. For alt tab
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0]  = LAYOUT_ortho_4x12(
     KC_TAB,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,   KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,    KC_LBRC,
-    KC_ESC,  LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,   KC_H,    KC_J,  KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+    QK_GESC, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,   KC_H,    KC_J,  KC_K,    KC_L,   KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,   KC_N,    KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
     KC_LCTL, KC_LGUI,      KC_LALT,      TT(4),        TT(1),        KC_SPC, KC_BSPC, TT(3), TT(2),   OSL(4), KC_DEL,  KC_NO
   ),
   [1]  = LAYOUT_ortho_4x12(
-    QK_GESC, KC_EXLM,      KC_AT,        KC_HASH,      KC_DLR,       KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
+    KC_TAB,  KC_EXLM,      KC_AT,        KC_HASH,      KC_DLR,       KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
     KC_ESC,  LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), LALT(KC_LSFT), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_NO,
     KC_LSFT, LCTL(KC_Z),   LCTL(KC_X),   LCTL(KC_C),   LCTL(KC_V),   LCTL(KC_B),    KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_UP,   KC_ENT,
     KC_LCTL, KC_LGUI,      KC_LALT,      KC_TRNS,      KC_TRNS,      KC_SPC,        KC_DEL,  MO(5),   KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT
