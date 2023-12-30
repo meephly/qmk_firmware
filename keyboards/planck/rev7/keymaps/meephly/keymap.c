0cb138aaa06a627c5451af2a6fb64b04d1fe9eaf
Meephly <v.sherstyanykh@gmail.com>
2023-12-30 16:24:03 +0300

    change mouse keys to wasd like

--- keyboards/planck/rev7/keymaps/meephly/keymap.c
+++ keyboards/planck/rev7/keymaps/meephly/keymap.c
@@ -69,10 +69,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LGUI, KC_LALT, MO(6),   KC_TRNS, KC_SPC,  KC_BSPC, KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU, KC_MPLY
     ),
     [3]  = LAYOUT_ortho_4x12(
-        KC_TAB,  KC_NO,   KC_NO,   KC_BTN3, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_BTN3, KC_NO,   KC_NO,
-        KC_ESC,  KC_NO,   KC_NO,   KC_BTN2, KC_BTN1, KC_WH_U, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_NO,   KC_NO,
-        KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_WH_D, KC_BTN2, KC_BTN1, KC_ACL0, KC_ACL1, KC_ACL2, KC_NO,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_TRNS, MO(5),   KC_NO,   KC_BTN1, KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_N
+        KC_TAB,  KC_NO,   KC_NO,   KC_BTN3, KC_NO,   KC_NO, KC_NO,   KC_NO,   KC_MS_U, KC_BTN3, KC_NO,   KC_NO,
+        KC_ESC,  KC_NO,   KC_NO,   KC_BTN2, KC_BTN1, KC_NO, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_U, KC_NO,
+        KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_BTN2, KC_BTN1, KC_ACL0, KC_ACL1, KC_ACL2, KC_NO,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_TRNS, MO(5),   KC_NO, KC_BTN1, KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_N
     ),
     [4]  = LAYOUT_ortho_4x12(
         KC_NO,        KC_NO,      LALT(KC_F4), KC_NO,        KC_NO,      KC_NO,  KC_NUM, KC_7,    KC_8,  KC_9,    KC_MINS, KC_PSCR,
